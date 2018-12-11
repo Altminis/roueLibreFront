2b615e70a7710e33e3e429da07aa554b7ce40751
Jeremy Ptak <jeremy.ptak24@gmail.com>
2018-12-11 18:00:27 +0100

    formulaire de contact fonctionnel. Ajoute Email de contact dans le JSON

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, FormControl, Validators } from '@angular/forms';
 import { AuthService } from '../service/auth-service.service';
 import { Router } from '@angular/router';
+import { MessageContact } from '../model/MessageContact';
 
 @Component({
   selector: 'app-login',
--- src/app/service/message-contact.service.ts
+++ src/app/service/message-contact.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { MessageContact } from '../model/MessageContact';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MessageContactService {
 
-  constructor() { }
+  constructor(private http : HttpClient) { }
+
+  sendMessage(message : MessageContact) : Observable<any> {
+    return this.http.post(environment.backURL + '/messages', message);
+  }
 }
